a108d5ac9b01dee679d2d7bff23650778f8d3c5e
adrianitos <Adria@Ordinador>
2014-02-19 23:07:15 +0100

    Add

--- gen/com/amigosdejesus/jesusamigo/R.java
+++ gen/com/amigosdejesus/jesusamigo/R.java
@@ -21,12 +21,23 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int colega=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int bfb=0x7f020000;
+        public static final int bfbclick=0x7f020001;
+        public static final int big24=0x7f020002;
+        public static final int btw=0x7f020003;
+        public static final int btwclick=0x7f020004;
+        public static final int colega=0x7f020005;
+        public static final int effectfb=0x7f020006;
+        public static final int effecttw=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080005;
+        public static final int bfb=0x7f080003;
+        public static final int btw=0x7f080004;
         public static final int imageView1=0x7f080000;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -37,7 +48,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int phrase=0x7f050003;
+        public static final int title=0x7f050002;
     }
     public static final class style {
         /** 
--- src/com/amigosdejesus/jesusamigo/MainActivity.java
+++ src/com/amigosdejesus/jesusamigo/MainActivity.java
@@ -1,17 +1,19 @@
 package com.amigosdejesus.jesusamigo;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        
+        setContentView(R.layout.activity_main);  
     }
 
 
@@ -20,6 +22,16 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void onclickfb(View v){
+    	Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
+    }
+    
+    public void onclicktw(View v){
+    	Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
     }
     
 }
